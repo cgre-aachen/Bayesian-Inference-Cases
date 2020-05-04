fix bug in Hessian MCMC

Polygon_gravity_mode/GaussianProcess.py
@@ -10,15 +10,27 @@ psd_kernels = tfp.math.psd_kernels
 class GaussianProcess2Dlayer:
 
     def __init__(self, Range, depth, Number_para, thickness=None, number_of_fixpoints=10):
+        """[summary]
+
+        Arguments:
+            Range {[type]} -- [description]
+            depth {[type]} -- [description]
+            Number_para {[type]} -- [description]
+            amplitude -- Signal variance σ2
+
+        Keyword Arguments:
+            thickness {[type]} -- [description] (default: {None})
+            number_of_fixpoints {int} -- [description] (default: {10})
+        """
         self.number_of_fixpoints = number_of_fixpoints
         self.Range = Range
         self.depth = depth
         self.Number_para = Number_para
         self.thickness = thickness
         self.fix_point1 = tf.transpose(tf.stack([tf.linspace(
-            self.Range[0]-200, self.Range[0]-10, self.number_of_fixpoints), self.depth*tf.ones(self.number_of_fixpoints, dtype=tf.float64)]))
+            self.Range[0] - 200, self.Range[0] - 10, self.number_of_fixpoints), self.depth * tf.ones(self.number_of_fixpoints, dtype=tf.float64)]))
         self.fix_point2 = tf.transpose(tf.stack([tf.linspace(
-            self.Range[1]+10, self.Range[1]+200, self.number_of_fixpoints), self.depth*tf.ones(self.number_of_fixpoints, dtype=tf.float64)]))
+            self.Range[1] + 10, self.Range[1] + 200, self.number_of_fixpoints), self.depth * tf.ones(self.number_of_fixpoints, dtype=tf.float64)]))
 
         self.amplitude = tfp.util.TransformedVariable(
             2, tfb.Exp(), dtype=tf.float64, name='amplitude')
@@ -35,6 +47,7 @@ class GaussianProcess2Dlayer:
         '''
         Arguments:
             kernel: trained GP kernal
+            k(x, y) = amplitude**2 * exp(-||x - y||**2 / (2 * length_scale**2))
 
         return:
             GP interpolated model index and model psition
@@ -49,11 +62,11 @@ class GaussianProcess2Dlayer:
             [tf.concat([self.fix_point1, points], axis=0), self.fix_point2], axis=0)
 
         observation_index_points = tf.reshape(
-            points[:, 0], [self.Number_para+2*self.number_of_fixpoints, 1])
+            points[:, 0], [self.Number_para + 2 * self.number_of_fixpoints, 1])
 
         # x-index where we want to interpolate
         model_index = tf.expand_dims(tf.linspace(
-            observation_index_points[0, 0], observation_index_points[-1, 0], self.resolution*self.Number_para+4), axis=1)
+            observation_index_points[0, 0], observation_index_points[-1, 0], self.resolution * self.Number_para + 4), axis=1)
 
         gprm = tfd.GaussianProcessRegressionModel(
             kernel=self.kernel,
@@ -67,7 +80,7 @@ class GaussianProcess2Dlayer:
         # model_complete contains the extended polygon used to do gravity calculation
         model_position_complete = tf.reshape([tf.concat([model_position - self.thickness,
                                                          tf.reverse(model_position, axis=[-1])], axis=-1)],
-                                             shape=[model_position.shape[0]*2, 1])
+                                             shape=[model_position.shape[0] * 2, 1])
 
         model_index_complete = tf.concat(
             [model_index, tf.reverse(model_index, axis=[0])], axis=0)

Polygon_gravity_mode/Grav_polygon.py
@@ -47,8 +47,8 @@ class Gravity_Polygon(tf.Module):
             self.Range, self.depth, self.Number_para, self.thickness)
 
     def A(self, x1, z1, x2, z2):
-        numerator = (x2-x1)*(x1*z2-x2*z1)
-        denominator = (x2-x1)**2 + (z2-z1)**2
+        numerator = (x2 - x1) * (x1 * z2 - x2 * z1)
+        denominator = (x2 - x1)**2 + (z2 - z1)**2
         return divide(numerator, denominator)
 
     def B(self, x1, z1, x2, z2):
@@ -58,7 +58,7 @@ class Gravity_Polygon(tf.Module):
         p1, p2 : int, position
 
         '''
-        return divide((z1-z2), (x2-x1))
+        return divide((z1 - z2), (x2 - x1))
 
     def theta_new(self, xn, zn):
 
@@ -66,7 +66,7 @@ class Gravity_Polygon(tf.Module):
 
         m = tf.where(m < 0, m + pi, m)
 
-        m = tf.where(m == 0, m + pi/2, m)
+        m = tf.where(m == 0, m + pi / 2, m)
 
         return m
 
@@ -78,13 +78,13 @@ class Gravity_Polygon(tf.Module):
         theta1 = self.theta_new(x1, z1)
         theta2 = self.theta_new(x2, z2)
 
-        r1 = (tf.sqrt(x1**2.+z1**2.))
-        r2 = (tf.sqrt(x2**2.+z2**2.))
+        r1 = (tf.sqrt(x1**2. + z1**2.))
+        r2 = (tf.sqrt(x2**2. + z2**2.))
 
         _A = self.A(x1, z1, x2, z2)
         _B = self.B(x1, z1, x2, z2)
 
-        Z_result = _A*((theta1-theta2)+_B*tf.math.log(divide(r1, r2)))
+        Z_result = _A * ((theta1 - theta2) + _B * tf.math.log(divide(r1, r2)))
 
         return Z_result
 
@@ -125,11 +125,8 @@ class Gravity_Polygon(tf.Module):
 
         # gravitational constant  m^3 kg ^-1 s^-2
         G = constant64(6.67 * 10**(-11))
-        gravity = 2*G*self.rho * \
+        gravity = 2 * G * self.rho * \
             tf.reduce_sum(-self.Z_new(x1, z1, x2, z2), axis=1)
-
-        # print('tracing')
-        # tf.print('executing')
 
         return tf.squeeze(gravity)
 
@@ -140,7 +137,7 @@ class Gravity_Polygon(tf.Module):
         self.cov = cov
 
     @tf.function
-    def joint_log_post(self, Data, _control_position):
+    def joint_log_post(self, Data, control_position):
         """[summary]
 
         Arguments:
@@ -159,9 +156,9 @@ class Gravity_Polygon(tf.Module):
         # define likelihood
 
         _control_index = tf.linspace(
-            self.Range[0], self.Range[1], _control_position.shape[0])
+            self.Range[0], self.Range[1], tf.shape(control_position)[0])
 
-        __x, __z = self.gp.GaussianProcess(_control_index, _control_position)
+        __x, __z = self.gp.GaussianProcess(_control_index, control_position)
 
         Gm_ = self.calculate_gravity(__x, __z)
 
@@ -170,9 +167,9 @@ class Gravity_Polygon(tf.Module):
             scale_tril=tf.linalg.cholesky(self.cov))
 
         # return the posterior probability
-        return (mvn_prior.log_prob(_control_position)
+        return (mvn_prior.log_prob(control_position)
                 + mvn_likelihood.log_prob(Data))
 
     @tf.function
-    def negative_log_posterior(self, Data, _control_position):
-        return -self.joint_log_post(Data, _control_position)
+    def negative_log_posterior(self, Data, control_position):
+        return -self.joint_log_post(Data, control_position)

Polygon_gravity_mode/HessianMCMC.py
@@ -94,9 +94,11 @@ class HessianMCMC():
 
         return m_proposed
 
-    def run_chain_hessian(self):
+    def run_chain_hessian(self,Hess = None):
 
-        self.Full_Hessian()
+        if Hess is None:
+            self.Full_Hessian()
+        else: self.Hessian = Hess
         self.Laplace_appro()
 
         burn_in = self.number_burnin
@@ -104,6 +106,7 @@ class HessianMCMC():
         k = 0
         accepted = []
         rejected = []
+        samples = []
 
         m_current = self.mu_init  # init m
 
@@ -115,10 +118,11 @@ class HessianMCMC():
                 m_current = m_proposed
                 if k > burn_in:
                     accepted.append(m_proposed.numpy())
+                    samples.append(m_proposed.numpy())
             else:
                 m_current = m_current
                 rejected.append(m_proposed.numpy())
-
+                samples.append(m_current.numpy())
         self.acceptance_rate = np.shape(accepted)[0]/self.number_sample
 
-        return accepted, rejected
+        return accepted, rejected, samples